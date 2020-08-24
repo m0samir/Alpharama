46470a526496a48eecf4edc1cd26ee448f7ad63d
Sai-Raj-Ltd <57390445+Sai-Raj-Ltd@users.noreply.github.com>
2020-08-24 19:36:48 +0300

    Update stock_move_line.py

--- insert_custom_fields/models/stock_move_line.py
+++ insert_custom_fields/models/stock_move_line.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from collections import defaultdict
+
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
@@ -11,99 +13,70 @@ class StockMove(models.Model):
     no_of_pieces = fields.Float(string="Pieces", store=True)
     mno_of_pieces = fields.Float(string="Pieces", store=True)
 
-    @api.onchange('lot_id')
-    def _pieces_auto_populate(self):
-        for rec in self:
-            if rec.lot_id:
-                rec.mno_of_pieces = rec.lot_id.no_of_pieces
-   
 
-class StockPickingValidation(models.Model):
-    _inherit = "stock.picking"
-    
-    # def button_validate(self):
-        # for rec in self.move_line_ids_without_package:
-# #             if rec.lot_id and rec.mno_of_pieces > rec.lot_id.no_of_pieces and rec.product_id:
-# #                 rec.mno_of_pieces = rec.lot_id.no_of_pieces
-# #                 raise ValidationError("Available Pieces are %s" % rec.lot_id.no_of_pieces)
-            
-            # if rec.lot_id and rec.mno_of_pieces <= rec.lot_id.no_of_pieces and rec.product_id:
-                # rec.lot_id.no_of_pieces = rec.lot_id.no_of_pieces - rec.mno_of_pieces
-                
-        # self.ensure_one()
-        # if not self.move_lines and not self.move_line_ids:
-            # raise UserError(_('Please add some items to move.'))
 
-        # # Clean-up the context key at validation to avoid forcing the creation of immediate
-        # # transfers.
-        # ctx = dict(self.env.context)
-        # ctx.pop('default_immediate_transfer', None)
-        # self = self.with_context(ctx)
+    def _action_done(self, cancel_backorder=False):
+        self.filtered(lambda move: move.state == 'draft')._action_confirm()  # MRP allows scrapping draft moves
+        moves = self.exists().filtered(lambda x: x.state not in ('done', 'cancel'))
+        moves_todo = self.env['stock.move']
 
-        # # add user as a follower
-        # self.message_subscribe([self.env.user.partner_id.id])
+        # Cancel moves where necessary ; we should do it before creating the extra moves because
+        # this operation could trigger a merge of moves.
+        for move in moves:
+            if move.quantity_done <= 0:
+                if float_compare(move.product_uom_qty, 0.0, precision_rounding=move.product_uom.rounding) == 0 or cancel_backorder:
+                    move._action_cancel()
 
-        # # If no lots when needed, raise error
-        # picking_type = self.picking_type_id
-        # precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        # no_quantities_done = all(float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in self.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
-        # no_reserved_quantities = all(float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for move_line in self.move_line_ids)
-        # if no_reserved_quantities and no_quantities_done:
-            # raise UserError(_('You cannot validate a transfer if no quantites are reserved nor done. To force the transfer, switch in edit more and encode the done quantities.'))
+        # Create extra moves where necessary
+        for move in moves:
+            if move.state == 'cancel' or move.quantity_done <= 0:
+                continue
 
-        # if picking_type.use_create_lots or picking_type.use_existing_lots:
-            # lines_to_check = self.move_line_ids
-            # if not no_quantities_done:
-                # lines_to_check = lines_to_check.filtered(
-                    # lambda line: float_compare(line.qty_done, 0,
-                                               # precision_rounding=line.product_uom_id.rounding)
-                # )
+            moves_todo |= move._create_extra_move()
 
-            # for line in lines_to_check:
-                # product = line.product_id
-                # if product and product.tracking != 'none':
-                    # if not line.lot_name and not line.lot_id:
-                        # raise UserError(_('You need to supply a Lot/Serial number for product %s.') % product.display_name)
+        moves_todo._check_company()
+        # Split moves where necessary and move quants
+        for move in moves_todo:
+            # To know whether we need to create a backorder or not, round to the general product's
+            # decimal precision and not the product's UOM.
+            rounding = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+            if float_compare(move.quantity_done, move.product_uom_qty, precision_digits=rounding) < 0:
+                # Need to do some kind of conversion here
+                qty_split = move.product_uom._compute_quantity(move.product_uom_qty - move.quantity_done, move.product_id.uom_id, rounding_method='HALF-UP')
+                new_move = move._split(qty_split)
+                move._unreserve_initial_demand(new_move)
+                if cancel_backorder:
+                    self.env['stock.move'].browse(new_move)._action_cancel()
+        moves_todo.mapped('move_line_ids').sorted()._action_done()
+        # Check the consistency of the result packages; there should be an unique location across
+        # the contained quants.
+        for result_package in moves_todo\
+                .mapped('move_line_ids.result_package_id')\
+                .filtered(lambda p: p.quant_ids and len(p.quant_ids) > 1):
+            if len(result_package.quant_ids.filtered(lambda q: not float_is_zero(abs(q.quantity) + abs(q.reserved_quantity), precision_rounding=q.product_uom_id.rounding)).mapped('location_id')) > 1:
+                raise UserError(_('You cannot move the same package content more than once in the same transfer or split the same package into two location.'))
+        picking = moves_todo.mapped('picking_id')
+        #import pdb;pdb.set_trace()
+        moves_todo.write({'state': 'done', 'date': fields.Datetime.now()})
+        for line in moves_todo:
+            if line.state=='done':
+                intital_product=line.product_id.pt_no_of_pieces
+                line.product_id.pt_no_of_pieces=line.mno_of_pieces+intital_product
 
-        # # Propose to use the sms mechanism the first time a delivery
-        # # picking is validated. Whatever the user's decision (use it or not),
-        # # the method button_validate is called again (except if it's cancel),
-        # # so the checks are made twice in that case, but the flow is not broken
-        # sms_confirmation = self._check_sms_confirmation_popup()
-        # if sms_confirmation:
-            # return sms_confirmation
+        move_dests_per_company = defaultdict(lambda: self.env['stock.move'])
+        for move_dest in moves_todo.move_dest_ids:
+            move_dests_per_company[move_dest.company_id.id] |= move_dest
+        for company_id, move_dests in move_dests_per_company.items():
+            move_dests.sudo().with_context(force_company=company_id)._action_assign()
 
-        # if no_quantities_done:
-            # view = self.env.ref('stock.view_immediate_transfer')
-            # wiz = self.env['stock.immediate.transfer'].create({'pick_ids': [(4, self.id)]})
-            # return {
-                # 'name': _('Immediate Transfer?'),
-                # 'type': 'ir.actions.act_window',
-                # 'view_mode': 'form',
-                # 'res_model': 'stock.immediate.transfer',
-                # 'views': [(view.id, 'form')],
-                # 'view_id': view.id,
-                # 'target': 'new',
-                # 'res_id': wiz.id,
-                # 'context': self.env.context,
-            # }
+        # We don't want to create back order for scrap moves
+        # Replace by a kwarg in master
+        if self.env.context.get('is_scrap'):
+            return moves_todo
 
-        # if self._get_overprocessed_stock_moves() and not self._context.get('skip_overprocessed_check'):
-            # view = self.env.ref('stock.view_overprocessed_transfer')
-            # wiz = self.env['stock.overprocessed.transfer'].create({'picking_id': self.id})
-            # return {
-                # 'type': 'ir.actions.act_window',
-                # 'view_mode': 'form',
-                # 'res_model': 'stock.overprocessed.transfer',
-                # 'views': [(view.id, 'form')],
-                # 'view_id': view.id,
-                # 'target': 'new',
-                # 'res_id': wiz.id,
-                # 'context': self.env.context,
-            # }
+        if picking and not cancel_backorder:
+            picking._create_backorder()
+        return moves_todo
 
-        # # Check backorder should check for other barcodes
-        # if self._check_backorder():
-            # return self.action_generate_backorder_wizard()
-        # self.action_done()
-        # return
+
+ 
