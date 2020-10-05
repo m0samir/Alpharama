d9ab83c9f32449ad82b6112e8c0ede0f6666208b
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-10-05 16:01:47 +0300

    Add files via upload

---
+++ salt_alpharama/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Salt Solution for Alpharama',
+    'version': '12.0.1.0.0',
+    "author": "Thilak",
+    'license': 'AGPL-3',
+    'category': 'Stock',
+    'website': '',
+	'depends': ['stock'],
+    'data': [
+        'views/views.xml',
+    ],
+
+    'installable': True,
+	'auto_install': False,
+}
