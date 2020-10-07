7a40411037e084fcc7259396df5cea8d2a4cefbd
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-10-08 00:45:01 +0530

    Update models.py

--- salt_alpharama/models/models.py
+++ salt_alpharama/models/models.py
@@ -14,3 +14,17 @@ class LandedCost(models.Model):
 
     internal_transfer = fields.Many2one('stock.picking', domain="[('name', 'like', '/INT/')]",
                                    string='Internal Transfer', states={'done': [('readonly', True)]})
+    
+    def compute_internal_transfer(self):
+        for rec in self:
+            if rec.internal_transfer:
+                rec.cost_lines.unlink()
+                for record in rec.internal_transfer.move_line_ids_without_package:
+                    self.env['stock.landed.cost.lines'].create({
+                    'product_id': record.product_id.id,
+                    'name': record.product_id.name or '',
+                    'split_method': 'equal',
+                    'price_unit': record.product_id.standard_price * record.qty_done or 0.0,
+                    'account_id': record.product_id.property_account_expense_id.id or       record.product_id.categ_id.property_account_expense_categ_id.id,
+                    'cost_id': rec.id,
+                    })  
