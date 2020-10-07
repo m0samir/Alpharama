7c1e503ecf86be83867b98c71d33ad041912cb48
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-10-07 11:36:31 +0530

    Update models.py

--- salt_alpharama/models/models.py
+++ salt_alpharama/models/models.py
@@ -9,5 +9,5 @@ from odoo.exceptions import UserError
 class LandedCost(models.Model):
     _inherit = 'stock.landed.cost'
 
-    internal_transfer = fields.Many2one('stock.picking', domain="[('number', 'like', 'BILL')]",
+    internal_transfer = fields.Many2one('stock.picking', domain="[('name', 'like', '/INT/')]",
                                    string='Internal Transfer', states={'done': [('readonly', True)]})
