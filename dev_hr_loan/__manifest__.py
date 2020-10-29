dae82cba9e344a6858a6823121c56911651c9715
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-10-29 09:45:00 +0300

    Update __manifest__.py

--- dev_hr_loan/__manifest__.py
+++ dev_hr_loan/__manifest__.py
@@ -4,11 +4,11 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2015 DevIntelle Consulting Service Pvt.Ltd (<http://www.devintellecs.com>).
 #
-<<<<<<< HEAD
+# <<<<<<< HEAD
 #    For Module Support : devintelle@gmail.com  or Skype : devintelle
 =======
 #    For Module Support : devintelle@gmail.com  or Skype : devintelle 
->>>>>>> 6559eaf031b8eb1c6a34277b717674ce81ff44ca
+# >>>>>>> 6559eaf031b8eb1c6a34277b717674ce81ff44ca
 #
 ##############################################################################
 
