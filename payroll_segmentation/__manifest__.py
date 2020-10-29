984f15c2618dd0d19bfcfeedd745d0c85d207c8b
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-10-29 12:41:01 +0300

    Add files via upload

---
+++ payroll_segmentation/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Payroll Segmentation",
+
+    'summary': """
+        Payroll Segmentation
+        """,
+
+    'description': """
+        Payroll Segmentation
+    """,
+
+    'author': "Thilak Mannava",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Extra Tools',
+    'version': '0.1',
+
+    'depends': ['hr_ke'],
+    'data': [
+        'views/views.xml',
+        'security/security.xml',
+    ],
+}
