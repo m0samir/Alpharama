cd83b4789afbcec2540a6e4ebf7546991c6edaab
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-11-05 23:22:48 +0530

    Update models.py

--- product_over_heads/models/models.py
+++ product_over_heads/models/models.py
@@ -3,6 +3,7 @@ from dateutil import relativedelta
 from itertools import groupby
 from operator import itemgetter
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError,UserError
 
 
 class ProductOverHeads(models.Model):
@@ -12,19 +13,32 @@ class ProductOverHeads(models.Model):
     
     from_date = fields.Date(string="From Date", required=True, track_visibility='onchange')  
     to_date = fields.Date(string="To Date", required=True, track_visibility='onchange')
-    total_amount = fields.Float("Total Amount", compute="_onchange_product_oh_line_ids", readonly=True, track_visibility='onchange')
-    total_per_sft = fields.Float("Total Per SFT Amount", compute="_onchange_product_oh_line_ids", readonly=True, track_visibility='onchange')
+    total_amount = fields.Float("Total Amount", readonly=True, track_visibility='onchange')
+    total_per_sft = fields.Float("Total Per SFT Amount", readonly=True, track_visibility='onchange')
+    total_sft = fields.Float("Total SFT", required=False, track_visibility='onchange')
+    overhead_product = fields.Many2one("product.template", required=False, track_visibility='onchange')
     product_oh_line_ids = fields.One2many('product.overheads.line', "product_overheads_id", copy=True)
 
-    def _onchange_product_oh_line_ids(self):
-        if self.product_oh_line_ids:
-            self.total_amount = 0
-            self.total_per_sft = 0
-            for record in self:
-                for rec in record.product_oh_line_ids:
-                    record.total_amount += rec.amount
-                    record.total_per_sft += rec.per_sft
+    @api.depends('from_date','to_date')
+    def _date_validations(self):
+        for rec in self:
+            rec.total_customer_pay_amount = sum(
+                line.receiving_amt for line in rec.invoice_customer_payments)
             
+    def update_product_cost(self):
+        if not self.overhead_product:
+            raise ValidationError(_('Please select "Overhead Product"'))
+        if not self.total_sft or self.total_sft == 0:
+            raise ValidationError(_('Please enter "Total SFT" Value'))
+        self.total_amount = 0
+        self.total_per_sft = 0
+        for record in self:
+            for rec in record.product_oh_line_ids:
+                rec.per_sft = rec.amount / record.total_sft
+                record.total_amount += rec.amount
+                record.total_per_sft += rec.per_sft
+        self.overhead_product.standard_price = self.total_per_sft
+        
             
 class ProductOverHeadsLine(models.Model):
     _name = 'product.overheads.line'
@@ -33,7 +47,7 @@ class ProductOverHeadsLine(models.Model):
     product_overheads_id = fields.Many2one("product.overheads")
     overhead_id = fields.Many2one('overhead.overhead', string="Overhead", required=True)
     amount = fields.Float(string="Amount", required=True)
-    per_sft = fields.Float(string="Per SFT Amount", required=True)
+    per_sft = fields.Float(string="Per SFT Amount", readonly=True)
         
     
 class OverHeads(models.Model):
