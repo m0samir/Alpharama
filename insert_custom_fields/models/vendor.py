d4a88d7414d7ab2eec1ddef299b140b92665c310
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-11-06 16:55:49 +0530

    Update vendor.py

--- insert_custom_fields/models/vendor.py
+++ insert_custom_fields/models/vendor.py
@@ -1,1 +1,12 @@
+from datetime import datetime
+from dateutil import relativedelta
+from itertools import groupby
+from operator import itemgetter
 
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    vendor_type = fields.Selection([('Local', 'Local'), ('Import', 'Import'), ('Branch', 'Branch')], string='Vendor Type', required=True)
