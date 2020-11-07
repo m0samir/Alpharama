35e9fcc277e733f8a060c5aec8c50d90ddf1753c
Alpharama-Ltd <68687439+Alpharama-Ltd@users.noreply.github.com>
2020-11-08 00:18:51 +0530

    Update invoice.py

--- insert_custom_fields/models/invoice.py
+++ insert_custom_fields/models/invoice.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+from dateutil import relativedelta
+from itertools import groupby
+from operator import itemgetter
 
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+    _inherit = 'account.move'
+    
+    payment_tags = fields.Many2many('payment.tags', string='Payment Tags')
+    
+class PaymentTags(models.Model):
+    _name = 'payment.tags'
+    
+    name = fields.Char(string='Tag Name')
